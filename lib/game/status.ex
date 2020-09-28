0c975856c1ee9d706390a5713b6d7b0cc02419b9
Glauber Silva <glauber.lucio.silva@gmail.com>
2020-09-28 14:56:49 -0300

    add function to update game's state

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -11,4 +11,12 @@ defmodule ExMon.Game.Status do
     IO.puts("\n ===== Invalid move: #{move} ===== \n")
   end
 
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n ====== The player attacked the computer dealing #{damage} damage. ===== \n")
+  end
+
+  def print_move_message(:player, :attac, damage) do
+    IO.puts("\n ====== The computer attacked the player dealing #{damage} damage. ===== \n")
+  end
+
 end
